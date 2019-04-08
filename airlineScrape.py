ef61f03c4abd13351390cf46a1ce91436e4cd002
Nicholas Rucker <nick@Nicholass-MacBook-Pro-2.local>
2019-04-08 16:18:47 -0400

    Single request working A few major problems were encountered 1.  Using the urllib to request travel websites caused a request overload, when only one request was sent... 2.  Airline websites are very picky and make it super hard for users to scrape them. More to come.....

--- airlineScrape.py
+++ airlineScrape.py
@@ -1,27 +1,24 @@
 import requests
-import ssl
 import os
 import time
 import datetime
-from lxml import html
-
-import re
-import json
-import urllib
-import sys
-import os
-import logging
-
-if not os.path.exists("html_files"):
-	os.mkdir("html_files")
+from selenium import webdriver
 
 
-url = "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:chs,to:nyc,departure:04/16/2019TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com"
+if not os.path.exists("chs-nyc_html"):
+	os.mkdir("chs-nyc_html")
+
+browser = webdriver.Chrome()
+
+url = "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:chs,to:nyc,departure:04/16/2019TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com.php"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
-response = requests.get(url, headers=headers, verify=False)
 
-outputFile = open("hopefullyworkTry2.html", "wb")
+browser.get(url)
 
-time.sleep(25)
+time.sleep(5)
 
-outputFile.write(response.content)
+innerHTML = browser.execute_script("return document.body.innerHTML")
+
+outputFile = open("chs-nyc_html/seleniumScrape.html", "w")
+
+outputFile.write(innerHTML) 
