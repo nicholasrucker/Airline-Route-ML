15d4644a0ba13fe02b18103c076dcc4d0f643c00
Nicholas Rucker <nick@Nicholass-MacBook-Pro-2.local>
2019-04-08 16:22:59 -0400

    Oh yeah, I changed the website request to one that uses selenium.  The Expedia website only loaded in 4 results in the HTML and the rest of the results were loaded via javascript.  Since python cannot direclty execute this, I resorted to using selenium which uses a popup browser to get the HTML

--- airlineScrape.py
+++ airlineScrape.py
@@ -10,8 +10,8 @@ if not os.path.exists("chs-nyc_html"):
 
 browser = webdriver.Chrome()
 
-url = "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:chs,to:nyc,departure:04/16/2019TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com.php"
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
+#url = "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:chs,to:nyc,departure:04/16/2019TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com.php"
+url = "https://skiplagged.com/flights/CHS/NYC/2019-04-16"
 
 browser.get(url)
 
