9848cf110b9a321f627b4d3195fb4a96cfaaf2a2
Nicholas Rucker <nick@Nicholass-MacBook-Pro-2.local>
2019-04-09 14:37:24 -0400

    Comments have been added Various comments about how everything works This version is not intended to run properly, but should serve as a guide/stepping stone to the later steps The comments at the bottom are personal notes The next version will probably incorporate the functionality for looping through all the routes Then after that I'll play around with the parsing

--- airlineScrape.py
+++ airlineScrape.py
@@ -4,21 +4,34 @@ import time
 import datetime
 from selenium import webdriver
 
+# This will check to see if there is a directory for the flight data HTML
+# If there is not one, one will be made
+if not os.path.exists("chs-destination_html"):
+	os.mkdir("chs-destination_html")
 
-if not os.path.exists("chs-nyc_html"):
-	os.mkdir("chs-nyc_html")
 
+# Here we are declaring our web browser as Chrome
+# This can be any browser you have installed on your computer that works with selenium
 browser = webdriver.Chrome()
 
-#url = "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:chs,to:nyc,departure:04/16/2019TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com.php"
-url = "https://skiplagged.com/flights/CHS/NYC/2019-04-16"
+# Temp URL with no real destination
+url = "https://www.expedia.com/Flights-Search?trip=oneway&leg1=from:chs,to:XXXXXXXXXXXX,departure:04/16/2019TANYT&passengers=adults:1,children:0,seniors:0,infantinlap:Y&options=cabinclass%3Aeconomy&mode=search&origref=www.expedia.com.php"
 
+# This command uses selenium to open a browser to truly act like a deveoper 
+# It makes it so you can scrape webpages loaded by javaScript / have a lot of scraping preventions
 browser.get(url)
 
-time.sleep(5)
-
+# The script is executed which gets all the HTML from the website, even the part executed in JS
 innerHTML = browser.execute_script("return document.body.innerHTML")
 
-outputFile = open("chs-nyc_html/seleniumScrape.html", "w")
+# A file is created with the destination city in the file name and the HTML is written to it
+outputFile = open("chs-destination_html/<chs-desination>.html", "w")
+outputFile.write(innerHTML)
 
-outputFile.write(innerHTML) 
+
+
+
+
+#decision tree or multiclass
+
+#look at preprocessing
