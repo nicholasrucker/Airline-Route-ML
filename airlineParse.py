5d275ceb32f10f2e17b724b2dc8a0580bd15569b
Nicholas Rucker <nick@Nicholass-MacBook-Pro-2.local>
2019-04-10 19:05:42 -0400

    wooohoooo the parser is 'complete'
    I added functionality that allows it to parse all the flight routes using globs
    I am thinking about maybe adding the coordinate location of each airport...
    
    Ok now the next step is to create a map for all the airlines which will map each airline to a numerical key.  This is going to be key in the machine learning process since the airline string really means nothing to the computer.
    
    I guess I will have to parse the time of the flight too.  For that I'll just use a regular expression to get rid of the 'h' and 'm'.  Then I'll convert the travel time to a floating point number eg: 1h 30m ----> 1.5

--- airlineParse.py
+++ airlineParse.py
@@ -3,63 +3,72 @@ from bs4 import BeautifulSoup
 import glob
 import pandas as pd
 import re
+import glob
 
 if not os.path.exists("parsed_results"):
 	os.mkdir("parsed_results")
 
-dataFrame = pd.DataFrame()
+j = 1
 
-readHTML = open("chs-destination_html/<chs-desination2>.html", "r")
+for file in glob.glob("flight_html/*.html"):
 
-webSoup = BeautifulSoup(readHTML.read(), 'lxml')
-readHTML.close()
-
-i = 1
-
-for airlineContainer in webSoup.find_all('div', class_ = 'grid-container standard-padding'):
-
-	airline = airlineContainer.find('div', class_ = 'secondary-content overflow-ellipsis inline-children').span.text.strip()
-
-	price = airlineContainer.find('div', class_ = 'uitk-col price-details-container all-col-fill')['data-test-price-per-traveler']
-	price = str(price)
-	price = re.sub(r'[$]', '', price)
-
-	stops = airlineContainer.find('span', class_ = 'number-stops').text.strip()
-
-	travelTime = airlineContainer.find('span', class_ = 'duration-emphasis').text.strip()
-
-	departure = airlineContainer.find('div', class_ = 'secondary-content no-wrap').span.next_sibling.strip()[:3]
-
-	destination = airlineContainer.find('div', class_ = 'secondary-content no-wrap').span.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling
-	destination = str(destination)
-	destination = re.sub(r'[^A-Za-z]', '', destination)
-
-####################################################################################################################################################################################
-
-	if stops == "(Nonstop)":
-		stops = 0
-		print("Result", i, "has", stops,  "stops")
-	else:
-		stops = 1
-		print("Result", i, "has", stops,  "stop")
-
-	print("The airline is", airline)
-
-	print("The price of the ticket is", price)
-
-	print("The total travel time is", travelTime)
-
-	print("The flight leaves from", departure)
-
-	if stops == 1:
-		layover = airlineContainer.find('div', class_ = 'secondary-content no-wrap').span.next_sibling.next_sibling.span['data-stop-layover']
-	else:
-		layover = "N/A"
-
-	print("There is a layover in", layover)
-
-	print("The flight arrives in", destination)
-
+	print("Parsing file", file)
+	print()
 	print()
 
-	i = i + 1
+	dataFrame = pd.DataFrame()
+
+	readHTML = open(file, "r")
+
+	webSoup = BeautifulSoup(readHTML.read(), 'lxml')
+	readHTML.close()
+
+	i = 1
+
+	for airlineContainer in webSoup.find_all('div', class_ = 'grid-container standard-padding'):
+
+		airline = airlineContainer.find('div', class_ = 'secondary-content overflow-ellipsis inline-children').span.text.strip()
+
+		price = airlineContainer.find('div', class_ = 'uitk-col price-details-container all-col-fill')['data-test-price-per-traveler']
+		price = str(price)
+		price = re.sub(r'[$]', '', price)
+
+		stops = airlineContainer.find('span', class_ = 'number-stops').text.strip()
+
+		travelTime = airlineContainer.find('span', class_ = 'duration-emphasis').text.strip()
+
+		departure = airlineContainer.find('div', class_ = 'secondary-content no-wrap').span.next_sibling.strip()[:3]
+
+		destination = airlineContainer.find('div', class_ = 'secondary-content no-wrap').span.next_sibling.next_sibling.next_sibling.next_sibling.next_sibling
+		destination = str(destination)
+		destination = re.sub(r'[^A-Za-z]', '', destination)
+
+	####################################################################################################################################################################################
+
+		if stops == "(Nonstop)":
+			stops = 0
+			print("Result", i, "has", stops,  "stops")
+		else:
+			stops = 1
+			print("Result", i, "has", stops,  "stop")
+
+		print("The airline is", airline)
+
+		print("The price of the ticket is", price)
+
+		print("The total travel time is", travelTime)
+
+		print("The flight leaves from", departure)
+
+		if stops == 1:
+			layover = airlineContainer.find('div', class_ = 'secondary-content no-wrap').span.next_sibling.next_sibling.span['data-stop-layover']
+		else:
+			layover = "N/A"
+
+		print("There is a layover in", layover)
+
+		print("The flight arrives in", destination)
+
+		print()
+
+		i = i + 1
